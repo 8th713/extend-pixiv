bd21045c4a4495a06eeb453d98edba2d431c14d1
8th713 <8th713@gmail.com>
2015-06-05 22:21:57 +0900

    refactor(sort-worklist): use kefir

--- sort-worklist/index.js
+++ sort-worklist/index.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Sort the work list for pixiv
-// @version      0.2.1
+// @version      0.3.0
 // @description  Makes possible to toggle the asc/desc of work list.
 // @author       8th713
 // @namespace    http://8th713.tumblr.com/
@@ -10,61 +10,51 @@
 // @license      MIT
 // ==/UserScript==
 
-var Stream = require('stream');
+import Kefir from 'kefir';
+import {
+  ACTIVE_CLASS,
+  $$,
+  insertButton,
+  createFragment,
+  getApStream
+} from 'utils';
 
-var PAGERIZE_EVENTS = [
-  'AutoPagerize_DOMNodeInserted',
-  'AutoPatchWork.DOMNodeInserted',
-  'AutoPagerAfterInsert'
-];
+const MENU_ITEMS = '.menu-items';
+const IMAGE_ITEMS = '._image-items';
+const IMAGE_ITEM = '.image-item';
 
-function $$(selector, ctx) {
-  var collection = (ctx || document).querySelectorAll(selector);
+function createList() {
+  const pool = Kefir.pool();
+  const list = pool
+    .scan((acc, el) => acc.concat(el), [])
+    .debounce(0);
 
-  return [].slice.call(collection);
+  list.add = (el) => {
+    pool.plug(Kefir.constant(el));
+  };
+
+  return list;
 }
 
-function createBtn(text) {
-  var container = document.createElement('li');
-  var btn = document.createElement('a');
+const listStream = createList();
 
-  btn.textContent = text;
-  btn.href = '';
-  container.appendChild(btn);
-  document.querySelector('.menu-items').appendChild(container);
-
-  return btn;
-}
-
-var workList = PAGERIZE_EVENTS
-  .reduce(function(stream, eventType) {
-    return stream.merge(Stream.fromEventTarget(document.body, eventType));
-  }, new Stream())
-  .filter(function(event) {
-    return event.target.classList.contains('image-item');
+insertButton('古い順', MENU_ITEMS)
+  .map(event => {
+    return event.target.classList.contains(ACTIVE_CLASS);
   })
-  .map(function(event) {
-    return $$('.image-item', event.target);
+  .combine(listStream, (sorted, list) => {
+    if (sorted) {
+      return list.slice().reverse();
+    }
+    return list.slice();
   })
-  .scan([], function(arr, insertedNodes) {
-    return arr.concat(insertedNodes);
+  .map(createFragment)
+  .onValue(fragment => {
+    document.querySelector(IMAGE_ITEMS).appendChild(fragment);
   });
 
-Stream.fromEventTarget(createBtn('古い順'), 'click')
-  .map(function(event) {
-    event.preventDefault();
-    return event.target.classList.toggle('current');
-  })
-  .combine(workList, function(enabled, workList) {
-    if (enabled) { return workList.slice().reverse(); }
-    return workList;
-  })
-  .subscribe(function(data) {
-    var roots = $$('._image-items');
+getApStream()
+  .map(event => event.target)
+  .onValue(listStream.add);
 
-    data.forEach(function add(el, index) {
-      roots[~~(index / 20)].appendChild(el);
-    });
-  });
-
-workList.property($$('.image-item'));
+$$(IMAGE_ITEM).forEach(listStream.add);
